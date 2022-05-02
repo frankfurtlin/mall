d295dff7bc9150c8d4c5724724ca735e13d340c1
Frankfurtlin <@Linshujing1025>
2022-05-02 18:57:06 +0800

    商品删除接口开发

--- src/main/java/com/frankfurtlin/mall/controller/ProductController.java
+++ src/main/java/com/frankfurtlin/mall/controller/ProductController.java
@@ -2,6 +2,9 @@ package com.frankfurtlin.mall.controller;
 
 
 import com.frankfurtlin.mall.common.ApiRestResponse;
+import com.frankfurtlin.mall.exception.MallExceptionEnum;
+import com.frankfurtlin.mall.model.entity.Category;
+import com.frankfurtlin.mall.model.entity.Product;
 import com.frankfurtlin.mall.model.request.ProductAddReq;
 import com.frankfurtlin.mall.model.request.ProductUpdateReq;
 import com.frankfurtlin.mall.service.IProductService;
@@ -9,11 +12,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -45,6 +44,20 @@ public class ProductController {
     public ApiRestResponse<?> update(@ApiParam("更新商品实体类") @Valid @RequestBody ProductUpdateReq productUpdateReq) {
         iProductService.updateProduct(productUpdateReq);
 
+        return ApiRestResponse.success();
+    }
+
+    @ApiOperation("删除商品")
+    @PostMapping("/admin/delete")
+    public ApiRestResponse<?> delete(@ApiParam("商品id") @RequestParam Integer id) {
+        Product product = iProductService.getById(id);
+        if(product == null){
+            return ApiRestResponse.error(MallExceptionEnum.PRODUCT_ID_NOT_EXISTED);
+        }
+
+        if (!iProductService.removeById(id)) {
+            return ApiRestResponse.error(MallExceptionEnum.DATABASE_FAILED);
+        }
         return ApiRestResponse.success();
     }
 }
